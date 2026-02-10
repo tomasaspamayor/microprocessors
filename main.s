95603cabda65dc04fdaba79a87a72e3cd537a3a9
IC\ta2523 <ta2523@ic.ac.uk>
2026-02-10 11:12:16 +0000

    delay finished

--- main.s
+++ main.s
@@ -9,40 +9,55 @@ main:
 setup:	
 	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
+	
 	movlw	0x00
-	movwf	TRISB, A
+	movwf	TRISC, A    ; set port B to output 
 	goto	start
 	
 temmuztable:
-	db	'T','E','M','M','U','Z','T','U','M','A','Y'
-	counter EQU 0x00
-	table EQU 0x01
+	db	0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80
+	counter	EQU 0x00
+	table	EQU 0x01
 	align	2
+	    
+delay:	
+	movlw	10
+	movwf	0x22, A
 	
-delay:	decfsz	0x20, A
-	bra delay
-	bra delay 
+delay_outer:
+	movlw	0xFF
+	movwf	0x20, A
+
+delay_inner: 
+	decfsz	0x20, A
+	bra	delay_inner
+	
+	decfsz	0x21, A
+	bra	delay_outer
+	
 	return
     
 start:
-	lfsr	0, table    ; load table ino FSR0 
+	lfsr	0, table    ; load table address ino FSR0 
 	movlw	low highword(temmuztable)
 	movwf	TBLPTRU, A
 	movlw	low (temmuztable) 
 	movwf	TBLPTRL, A
 	movlw	high (temmuztable) 
 	movwf	TBLPTRH, A 
-	movlw	12		; 22 bytes to read
+	movlw	8		; n bytes to read
 	movwf 	counter, A
 
 loop:
-	tblrd*+
-	movff	TABLAT, PORTB
-	decfsz	counter, A	; count down to zero
+	    tblrd*+
+	movff	TABLAT, PORTC
+	
 	movlw	0xFF 
 	movwf	0x20, A 
 	call	delay
+	
+	decfsz	counter, A	; count down to zero
 	bra	loop		; keep going until finished
 	
-	goto	start
+	goto	0
 	end	main
