fed18ba51d23215511e15831c817d2cfa5906a97
IC\ta2523 <ta2523@ic.ac.uk>
2026-02-12 11:03:08 +0000

    fixed sampling on sawtooth

--- main.s
+++ main.s
@@ -20,18 +20,22 @@ start:
 	movwf   PORTD, A
 	movwf	0x01, A
 	movwf	0x02, A ; counter for sine function (runs to 8)
-	goto	sawtooth
+	goto	sine
 	
 sawtooth: ; sawtooth function generator
 	btg	PORTD, 0, A
 	
 	movlw	0x07 ; move 7 into w
-	cpfsgt	0x02, A ; skip loop if counter is greater than 7 
+	cpfsgt	0x02, A ; skip loop if counter is less than 7
+	bra	skiploop
+	
+	movlw	0x00
+	movwf	0x02, A ; reset counter
+	
 	incf	PORTJ, A
 	
-	nop
-	nop
-	btg	0x01, 0, A 
+skiploop:
+	incf	0x02, A 
 	bra	sawtooth
 	
 	
