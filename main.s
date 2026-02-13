ccc8fe6e1c7ce4f7898d9ae76d854311f8281adc
IC\ta2523 <ta2523@ic.ac.uk>
2026-02-13 11:05:42 +0000

    tried to read from a table

--- main.s
+++ main.s
@@ -3,25 +3,66 @@
 psect	code, abs
 main:
 	org 0x0
-	goto start
+	goto setup
 	
 	org 0x100
+
+setup: 
+	bcf CFGS
+	bcf EEPGD
+	goto start 
 	
-start: 
-    
+myTable:
+	db	'T','E','M','M','U','Z',' ', 'T', 'U', 'M', 'A', 'Y'
+	myArray EQU 0x20	; Address in RAM for data
+	counter EQU 0x10	; Address of counter variable
+	align	2		; ensure alignment of subsequent instructions 
+	
+Delay:
+	movlw   0xFF
+	movwf   0x50, A
+Delay_outer:
+	movlw   0x01
+	movwf   0x51, A
+Delay_mid:
+	movlw   0x01
+	movwf   0x52, A
+Delay_inner:
+	nop                 
+	decfsz  0x52, A
+	bra     Delay_inner
+	decfsz  0x51, A
+	bra     Delay_mid
+	decfsz  0x50, A
+	bra     Delay_outer
+	return
+	
+
+start:	
+	lfsr	0, myArray	; Load FSR0 with address in RAM	
+	movlw	low highword(myTable)	; address of data in PM
+	movwf	TBLPTRU, A	; load upper bits to TBLPTRU
+	movlw	high(myTable)	; address of data in PM
+	movwf	TBLPTRH, A	; load high byte to TBLPTRH
+	movlw	low(myTable)	; address of data in PM
+	movwf	TBLPTRL, A	; load low byte to TBLPTRL
+	movlw	12		; 22 bytes to read
+	movwf 	counter, A	; our counter register
+	
 	call    SPI_MasterInit
 	goto	loop
 	
-table:
-	db	"T","E","M","M","U","Z"
-	array	EQU 0x20
-	counter	EQU 0x10
-	align	2
+
 	
 loop:
-	movlw	0xAA
-	call SPI_MasterTransmit
-	bra loop
+	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
+	movf	TABLAT, W
+	call	SPI_MasterTransmit
+	decfsz	counter, A
+	bra	loop
+	
+	call	Delay
+	goto	0
 	
 
 SPI_MasterInit:
@@ -38,7 +79,7 @@ SPI_MasterTransmit:
 	movwf	SSP2BUF, A
 	
 Wait_Transmit: 
-	btfss	PIR2, 5
+	btfss	PIR2, 5, A
 
 	bra	Wait_Transmit
 	bcf	PIR2, 5, A
